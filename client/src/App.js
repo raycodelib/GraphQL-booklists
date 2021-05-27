680111a6e4333a6cfa759d3f5983f75d716b2661
Ray <xielei8912@outlook.com>
2021-05-27 18:02:21 +1000

    connect frontend to backend with apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,40 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  // HttpLink,
+  // from,
+} from "@apollo/client";
+// import { onError } from "@apollo/client/link/error";
+
+import BookList from "./components/BookList";
+
+// const errorLink = onError(({ graphqlErrors, networkError }) => {
+//   if (graphqlErrors) {
+//     // graphqlErrors.map(({ message, location, path }) => {
+//     //   alert(`Graphql error: ${message}`);
+//     // });
+//     console.log(graphqlErrors);
+//   }
+// });
+// const link = from([
+//   new HttpLink({ uri: "http://localhost:5000/graphql" }),
+//   errorLink,
+// ]);
+
+const link = "http://localhost:5000/graphql";
+
+const client = new ApolloClient({
+  uri: link,
+  cache: new InMemoryCache(),
+});
+
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <ApolloProvider client={client}>
+      <BookList />
+    </ApolloProvider>
+  );
 }
 
 export default App;
---
+++ client/src/components/BookList.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { useQuery } from "@apollo/client";
+import { LOAD_BOOKS } from "../GraphQL/Queries";
+
+function BookList() {
+  const { loading, data } = useQuery(LOAD_BOOKS);
+  const [books, setBooks] = useState();
+  useEffect(() => {
+    if (data) {
+      setBooks(data.books);
+    } else {
+      console.log(`error data: ${data}`);
+    }
+  }, [data]);
+  if (loading) {
+    return <div>Loading books...</div>;
+  }
+  if (books) {
+    return (
+      <div>
+        <ul id="book-list">
+          {books.map((book) => {
+            return (
+              <li key={book.name}>
+                {book.name}---{book.genre}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return <div>Empty Books returned</div>;
+  }
+}
+
+export default BookList;
